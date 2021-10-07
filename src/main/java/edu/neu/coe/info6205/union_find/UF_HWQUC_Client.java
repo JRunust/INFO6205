2498da17a7d584a3a58cc65ed82669f3d0267597
runhou <hr574177@gmail.com>
2021-10-07 17:21:18 -0400

    homework

--- src/main/java/edu/neu/coe/info6205/union_find/UF_HWQUC_Client.java
+++ src/main/java/edu/neu/coe/info6205/union_find/UF_HWQUC_Client.java
@@ -5,11 +5,23 @@ import java.util.Scanner;
 
 public class UF_HWQUC_Client {
     public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+//        System.out.println(count(sc.nextInt()));
+//        sc.close();
+        int[] arr = {100,200,400,800,1600,3200,6400,12800,25600,51200,102400,204800,409600};
+        for(int i = 0;i < arr.length;i++){
+            System.out.println(i + " " + arr[i]+ " "+ count(arr[i])+" "+ count(arr[i]) / arr[i]);
+        }
+    }
+    public static int count(int n){
         Random random = new Random();
-        Scanner sc = new Scanner(System.in);
-        int numOfSites = sc.nextInt();
-       
-        new UF_HWQUPC().va
-//        System.out.println(numOfSites);
+        int numOfSites = n;
+        int count = 0;
+        UF_HWQUPC uf_hwqupc = new UF_HWQUPC(numOfSites);
+        while(uf_hwqupc.components() != 1){
+            count++;
+            uf_hwqupc.connect(random.nextInt(numOfSites),random.nextInt(numOfSites));
+        }
+        return count;
     }
 }
